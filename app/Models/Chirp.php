82d47256cefcc36f599369c8d87c869c3ab67ead
thibauttaglialatela <thibauttaglialatela@gmail.com>
2023-01-12 17:27:55 +0100

    création méthode ajout de messages

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Chirp extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'message',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
 }
